968cad706fdbd08234c08f6cf04ab5e4d1687c84
Margret-Jaison <77883030+Margret-Jaison@users.noreply.github.com>
2021-05-20 16:43:31 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    int choice=get_int("Enter type of pattern (1 or2)");
+    int n=get_int("Number of rows");
+    
+    switch(choice)
+     {
+       case 1:for(int i=1;i<=n;i++)
+              {
+                for(int j=n;j>=i;j--)
+                   {
+                   printf("#");
+                 
+                   }
+                printf("\n");
+              }
+             break;
+ 
+       case 2: for(int i=1;i<=n;i++)
+               {
+                 
+                 for(int k=i;k<=n;k++)
+                   {
+                   printf(" "); 
+                  
+                   }
+                 for(int j=1;j<=i;j++)
+                   {
+                    printf("#");
+                   }
+                   printf("\n");
+               }
+       
+              break;
+     default:break;  
+     }
+ }
+    }
+ }
+		
