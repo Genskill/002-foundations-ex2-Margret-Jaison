9862353af2b44bf448c9d62529b5997c3a4fceaf
Margret-Jaison <77883030+Margret-Jaison@users.noreply.github.com>
2021-05-20 16:19:44 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     int choice=get_int("Enter type of pattern (1 or2) ");
-    int n=get_int("Number of rows");
+    int n=get_int("Number of rows\n");
     switch(choice)
      {
        case 1:for(int i=1;i<=n;i++)
